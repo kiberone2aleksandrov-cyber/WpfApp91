ddad090e83e6dbc227a09d9e164f3c5879a78a8e
asdasdQWErty <kotovdmitrij100@gmail.com>
2025-11-23 13:39:56 +0300

    цй3укЦУ4А

--- WpfApp91/Window1.xaml.cs
+++ WpfApp91/Window1.xaml.cs
@@ -26,7 +26,7 @@ namespace WpfApp91
             WindowState = WindowState.Maximized;
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)//sdfsdf
+        private void Button_Click(object sender, RoutedEventArgs e)//sdfsdfЦУАЦ
         {
 
         }
